86b7f001bdc57c11cb7d5d43b8b8c92df90e1739
Ollie <ollie.hm@cantab.net>
2017-05-22 22:56:00 +0100

    Add soundcloud link

--- app/services/page.rb
+++ app/services/page.rb
@@ -9,7 +9,7 @@ class Page
 
   def self.generate_pages
     [
-      self.new(title: 'Find out more', link: '/about', sections: [self.new(title: 'The band', link: '/about#band', sections: [self.new(title: 'Third level', link: '/about#third')]), self.new(title: 'Lyrics', link: '/about#lyrics')]),
+      self.new(title: 'Find out more', link: '/about', sections: [self.new(title: 'The band', link: '/about#band'), self.new(title: 'Lyrics', link: '/about#lyrics')]),
       self.new(title: 'Listen', link: '/listen', sections: Song.all.decorate.map{|song| self.new(title: song.name, link: '/listen#' + song.anchor)}),
       self.new(title: 'Get in touch', link: '/contact')
     ]
